586f943fdd33f945d4356aaad2028383adcdf120
Abhilash Jena <ajena_be20@thapar.edu>
2026-08-20 09:00:57 +0530

    Adding starter files

---
+++ src/main/java/com/abhilash/spotly/SpotlyApplication.java
@@ -0,0 +1,13 @@
+package com.abhilash.spotly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhilash/spotly/SpotlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhilash.spotly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
